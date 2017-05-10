bb8df5839990f035e2e446b586ee0924a4b0b0d5
weichu <ms0701515@gmail.com>
2017-05-10 20:11:10 +0800

    123

--- weathermood-server/src/model/todos.js
+++ weathermood-server/src/model/todos.js
@@ -56,7 +56,7 @@ function createTodos(mood, text) {
 function accomplishTodo(id) {
 	return new Promise((resolve,reject) => {
 		// let accomplishTodoPost=null;
-		listTodos().then(todos => {
+		liodos().then(todos => {
 			todos.map(p => {
 				console.log(`P.ID : ${p.id}`);
 				if (p.id === id){
