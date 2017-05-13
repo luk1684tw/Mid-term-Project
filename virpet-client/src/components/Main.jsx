7dd310ad4874f92b2bb7734cd62059fb794eb293
weichu <ms0701515@gmail.com>
2017-05-14 01:12:39 +0800

    [update]create SingleEvent component

--- virpet-client/src/components/Main.jsx
+++ virpet-client/src/components/Main.jsx
@@ -11,27 +11,18 @@ import {
     NavLink,
     Input,
     Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
     Form,
     FormGroup,
     Label,
     FormText,
-    InputGroup,
-    InputGroupAddon
 } from 'reactstrap';
 import {connect} from 'react-redux';
-
+import SingleEvent from 'components/SingleEvent.jsx';
 import Today from 'components/Today.jsx';
 import Forecast from 'components/Forecast.jsx';
 import {setSearchText} from 'states/post-actions.js';
-import {toggleNavbar, eventTitle, eventDanger, eventDescript, eventGetStartDate, eventGetEndDate} from 'states/main-actions.js';
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
+import {toggleNavbar} from 'states/main-actions.js';
 import GoogleLogin from 'react-google-login';
-import 'react-datepicker/dist/react-datepicker.css';
 import './Main.css';
 
 class Main extends React.Component {
@@ -49,35 +40,16 @@ class Main extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            modal: false
-        };
         this.searchEl = null;
-        this.eventTitleEl = null;
-        this.eventStartDateEl = null;
-        this.eventEndDateEl = null;
-        this.eventDescriptEl = null;
         this.handleNavbarToggle = this.handleNavbarToggle.bind(this);
         this.handleSearchKeyPress = this.handleSearchKeyPress.bind(this);
         this.handleClearSearch = this.handleClearSearch.bind(this);
-        this.handleEventTitleChange = this.handleEventTitleChange.bind(this);
-        this.handleEventDescriptChange = this.handleEventDescriptChange.bind(this);
-        this.handleEventStartDateChange = this.handleEventStartDateChange.bind(this);
-        this.handleEventEndDateChange = this.handleEventEndDateChange.bind(this);
-        this.toggle = this.toggle.bind(this);
-    }
-    toggle() {
-        this.setState({
-            modal: !this.state.modal
-        });
     }
 
     render() {
         const responseGoogle = (response) => {
             console.log(response);
         }
-        const {eventTitleValue} = this.props;
-        // console.log(this.props.eventDescriptValue);
         return (
             <Router>
                 <div className='main'>
@@ -92,36 +64,8 @@ class Main extends React.Component {
                                             <NavLink tag={Link} to='/forecast'>提醒條</NavLink>
                                         </NavItem>
                                     </Nav>
-                                    <div>
-                                        <Button color="danger" onClick={this.toggle}>新增提醒</Button>
-                                        <Modal isOpen={this.state.modal} toggle={this.toggle} className='' backdrop={false}>
-                                            <ModalHeader toggle={this.toggle}>事件</ModalHeader>
-                                            <ModalBody>
-                                                <InputGroup>
-                                                    <InputGroupAddon>名稱</InputGroupAddon>
-                                                    <Input placeholder="段考爆炸" getRef={el => this.eventTitleEl = el} value={this.props.eventTitleValue} onChange={this.handleEventTitleChange}/>
-                                                </InputGroup>
-                                                <FormGroup>
-                                                    <Label for="exampleDate">開始日期</Label>
-                                                    <Input placeholder="date placeholder" type="date" name="date" id="exampleDate" getRef={el => this.eventStartDateEl = el} value={this.props.eventStartDate} onChange={this.handleEventStartDateChange}/>
-                                                    {/* <DatePicker selected={this.props.eventStartDate} onChange={this.handleEventStartDateChange}placeholderText="請點選輸入日期"/> */}
-                                                </FormGroup>
-                                                <FormGroup>
-                                                    <Label for="exampleDate">結束日期</Label>
-                                                    <Input type="date" name="date" id="exampleDate" getRef={el => this.eventEndDateEl = el} value={this.props.eventEndDate} onChange={this.handleEventEndDateChange}/>
-                                                    {/* <DatePicker selected={this.props.eventEndDate} onChange={this.handleEventEndDateChange}placeholderText="請點選輸入日期"/> */}
-                                                </FormGroup>
-                                                <FormGroup>
-                                                    <Label for="exampleText">描述</Label>
-                                                    <Input type="textarea" name="text" id="exampleText" getRef={el => this.eventDescriptEl = el} value={this.props.eventDescriptValue} onChange={this.handleEventDescriptChange} placeholder="明天考試QQ"/>
-                                                </FormGroup>
-                                            </ModalBody>
-                                            <ModalFooter>
-                                                <Button color="primary" onClick={this.toggle}>新增</Button>{' '}
-                                                <Button color="secondary" onClick={this.toggle}>取消</Button>
-                                            </ModalFooter>
-                                        </Modal>
-                                    </div>&nbsp;&nbsp;
+                                    <SingleEvent/>
+                                    &nbsp;&nbsp;
                                     <div>
                                         <GoogleLogin clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" buttonText="Login with Google" onSuccess={responseGoogle} onFailure={responseGoogle} className='btn btn-secondary' offline={false}></GoogleLogin>
                                     </div>
@@ -159,44 +103,9 @@ class Main extends React.Component {
         this.props.dispatch(setSearchText(''));
         this.searchEl.value = '';
     }
-    handleEventTitleChange(e) {
-        const text = e.target.value;
-        console.log('e.target.value = ' + e.target.value);
-        this.props.dispatch(eventTitle(text));
-        if (text && this.props.eventDanger) {
-            this.props.dispatch(eventDanger(false));
-        }
-    }
-    handleEventDescriptChange(e) {
-        const text = e.target.value;
-        //console.log('e.target.value = '+e.target.value);
-        this.props.dispatch(eventDescript(text));
-        if (text && this.props.eventDanger) {
-            this.props.dispatch(eventDanger(false));
-        }
-    }
-    handleEventStartDateChange(e){
-        //console.log(e.target);
-        const date = e.target.value;
-        console.log('Start Date: '+date);
-        this.props.dispatch(eventGetStartDate(date));
-        if(date && this.props.eventDanger){
-             this.props.dispatch(eventDanger(false));
-        }
-    }
-
-    handleEventEndDateChange(e){
-        const date = e.target.value;
-        console.log('End Date: '+date);
-        this.props.dispatch(eventGetEndDate(date));
-        if(date && this.props.eventDanger){
-             this.props.dispatch(eventDanger(false));
-        }
-    }
 }
 
 export default connect(state => ({
     ...state.main,
-    ...state.events,
     searchText: state.searchText
 }))(Main);
